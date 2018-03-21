2bf8c0ef0bb7d071378be9a3f681f98031096fd9
24Preacher <pedro98medeiros@gmail.com>
2018-03-21 15:12:21 +0000

    struct.h

---
+++ include/struct.h
@@ -0,0 +1,14 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <glib.h>
+#include "date.h"
+
+typedef struct Posts_data;
+typedef struct Posts_id;
+typedef struct Users;
+
+#endif
