3a3d6ac6eb7416c5240be6a341610a5f03171fbf
24Preacher <pedro98medeiros@gmail.com>
2018-04-11 15:19:38 +0100

    Comentários tags.c

--- src/tags.c
+++ src/tags.c
@@ -1,6 +1,13 @@
 #include "tags.h"
 
 // Lista ligada para guardar as tags
+/**
+\struct Lista Ligada que armazena as Tags
+@param nome Nome da Tag
+@param id_tag Identificador da Tag
+@param count Contador da Tags
+@param *prox Apontador para a proxima Tag
+*/
 struct tags {
 	char* nome;
 	long id_tag;
@@ -8,6 +15,13 @@ struct tags {
 	struct tags *prox;
 } Tags;
 
+/**
+\brief Função que cria uma estrutura das Tags
+@param nome Nome da Tag
+@param id_tag Identificador da Tag
+@param count Contador da Tags
+@param *prox Apontador para a proxima Tag
+*/
 Tags createTags (char* nome, long id, int c){
 	Tags t = malloc(sizeof(struct tags));
 	t->nome = nome;
@@ -18,27 +32,47 @@ Tags createTags (char* nome, long id, int c){
 	return t;
 }
 
-
+/**
+\brief Função que busca o nome da Tag
+@param Estrutura das Tags
+@returns O nome da Tag
+*/
 char* getNameTag (Tags t){
 	return mystrdup(t->nome);
 }
 
+/**
+\brief Função que busca o Identificador da Tag
+@param Estrutura das Tags
+@returns O ID da Tag
+*/
 long getIdTag (Tags t){
 	return t->id_tag;
 }
 
+/**
+\brief Função que busca o numero de vezes que uma Tag aparece
+@param Estrutura das Tags
+@returns O contador das Tags
+*/
 long getCountTags (Tags t){
 	return t->count;
 }
+
 
 void freeTags (Tags t){
 	 if (t)
 	 			free(t);
 }
 
+/**
+\brief Função que clona a estrutura das Tags
+@param Estrutura das Tags
+@returns A estrutura clonada das Tags
+*/
 Tags cloneTags (Tags t){
 	Tags r = malloc(sizeof(struct tags));
-		mystrdup(r -> nome);
+		mystrdup(r -> nome)
 		r -> id_tag;
 		r -> count;
 }
