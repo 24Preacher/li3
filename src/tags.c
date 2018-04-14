abcaa851d7af9160c24bae2a24d3d9eae6e4476f
FilipaPereira <lipavilela@gmail.com>
2018-04-14 19:21:01 +0100

    alterei struct das tags

--- src/tags.c
+++ src/tags.c
@@ -3,42 +3,34 @@
 // Lista ligada para guardar as tags
 struct tags {
 	char* nome;
-	long id_tag;
-	int count;
+	//long id_tag;
+	//int count;
 	struct tags *prox;
 } Tags;
 
-Tags createTags (char* nome, long id, int c){
+Tags createTags (char* nome){
 	Tags t = malloc(sizeof(struct tags));
 	t->nome = nome;
-	t->id_tag = id;
-	t->count = c;
 	t->prox = NULL;
 
 	return t;
 }
 
-
 char* getNameTag (Tags t){
 	return mystrdup(t->nome);
 }
-
+/*
 long getIdTag (Tags t){
 	return t->id_tag;
-}
-
-long getCountTags (Tags t){
-	return t->count;
-}
+} */
 
 void freeTags (Tags t){
 	 if (t)
-	 			free(t);
+	 	free(t);
 }
 
 Tags cloneTags (Tags t){
 	Tags r = malloc(sizeof(struct tags));
-		mystrdup(r -> nome);
-		r -> id_tag;
-		r -> count;
+	r -> nome = mystrdup(t -> nome);
+	return r;
 }
