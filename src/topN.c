82f047226c57f553bd8557047efbd28d2c46cb41
FilipaPereira <lipavilela@gmail.com>
2018-04-23 01:02:05 +0100

    estrutura para resolver as queries que requerem um top N

---
+++ src/topN.c
@@ -0,0 +1,103 @@
+
+struct topn {
+  long id;
+  int count;
+};
+
+TopN createTopN (long id, int c){
+  TopN novo = malloc(sizeof(struct topn));
+  novo->id = id;
+  novo->count = c;
+
+  return novo;
+}
+
+int getID_Top(TopN t){
+  return t->id;
+}
+
+int getCount(TopN t){
+  return t->count;
+}
+
+void setCount(TopN t, int c){
+  t->count = c;
+}
+
+void freeTop (TopN t){
+  free(t);
+}
+
+struct topArray {
+  int size;
+  int ocupados;
+  TopN top[];
+};
+
+ArrayTop createArrayTop (int size){
+  ArrayTop t = malloc(sizeof(struct topArray));
+  t->size = size;
+  t->ocupados = 0;
+  t->top = malloc(sizeof(struct topn) * size);
+  return t;
+}
+
+int getSizeTop (ArrayTop t){
+	 return t->size;
+}
+
+int getOcupados(ArrayTop t){
+	return t->ocupados;
+}
+
+void setOcupados(ArrayTop t, int o){
+	t->ocupados = o;
+}
+
+
+TopN getTop (ArrayTop t ,int i){
+  return t->top[i];
+}
+
+void setTop(ArrayTop t, TopN n, int i){
+  t->top[i]= n;
+}
+
+void freeArrayTop(ArratTop t){
+  free(t);
+}
+
+
+void insereTop (ArrayTop t, TopN n){
+  int i = 0;
+  int tam = getSizeTop(t);
+  int ocup = getOcupados(t);
+  int cont = getCount(n);
+
+  if(ocup == tam){
+    TopN ultimo = getTop(t, tam);
+    if (cont > getCount(ultimo)){
+          while(i < tam && getCount(getTop(t, i)) >= cont) i++;
+          pos = i;
+          for(int j = ocup-1; j > i; j--){
+            TopN anterior = getTop(t, j-1);
+            setTop(t, n, j);
+          }
+          setTop(t, n, pos);
+        }
+  }
+  else{
+    while(i < ocup && getCount(getTop(t, i)) >= cont) i++;
+    if(i == ocup) setTop(t, n, i);
+    else{
+      pos = i;
+      for(int k = ocup; k > i; k--){
+        TopN anterior = getTop(t, k-1);
+        setTop(t, n, k);
+      }
+      setTop(t, n, pos);
+      ocup++;
+      setOcupados(t, ocup);
+    }
+  }
+}
