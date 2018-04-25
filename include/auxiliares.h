8bcbc717456402685835ba3902f501a564a4dd5b
FilipaPereira <lipavilela@gmail.com>
2018-04-25 23:30:31 +0100

    .h das auxiliares

---
+++ include/auxiliares.h
@@ -0,0 +1,28 @@
+#ifndef __AUXILIARES__
+#define __AUXILIARES__
+
+
+
+#include "data.h"
+#include "userdata.h"
+#include "listaposts.h"
+#include "parser.h"
+#include "postsD.h"
+#include "postsID.h"
+#include "tags.h"
+#include "users.h"
+#include "string.h"
+#include <glib.h>
+#include "topN.h"
+
+
+void percorre(ArrayPosts p, ArrayTop t);
+gboolean incrementaPar (Posts_D p, UserDataPar u);
+gboolean checkTags(Posts_D p, UserDataTag u);
+gboolean topScore(Posts_D p, UserDataTop top);
+int pertenceTitulo(char* palavra, Posts_D p);
+gboolean temPalavra(Posts_D p, UserDataTitle u);
+int valResposta(Posts_D p, int rep);
+gboolean melhor(Posts_D p, UserDataRes u);
+
+#endif
--- include/data.h
+++ include/data.h
@@ -7,9 +7,10 @@
 #include <glib.h>
 #include "date.h"
 
+
 typedef struct data* Data;
 
-Data createData (int dia, int mes, int ano, int hora, int min);
+Data createData (int dia, int mes, int ano, int hora, int min, int seg, int mseg);
 
 int getDia (Data d);
 
@@ -21,6 +22,22 @@ int getHora (Data d);
 
 int getMinutos (Data d);
 
+int getSegundos(Data d);
+
+int getmSeg(Data d);
+
 void freeData (Data d);
+
+Data cloneData (Data d);
+
+gint compareMyData(Data a, Data b);
+
+int compareDate(Date a, Date b);
+
+Date converteData(Data a);
+
+int dataIgual(Date a, Data b);
+
+
 
 #endif
