80b85d4266d48a2437236cc78b774f547166e35f
HelenaMartins <lenamartiins@gmail.com>
2018-06-10 23:32:26 +0100

    ComparadorDataPostDec

--- proj-java/src/main/java/engine/ComparadorDataPost.java
+++ proj-java/src/main/java/engine/ComparadorDataPost.java
@@ -2,18 +2,17 @@ package engine;
 
 import java.util.Comparator;
 
-public class ComparadorDataPostDec implements Comparator<Posts> {
+public class ComparadorDataPost  implements Comparator<Posts> {
 
-    public int compare(Posts p1, Posts p2) {
+    public int compare(Posts p1, Posts p2){
         int res;
 
-        if (p1.getData().isAfter(p2.getData()))
-            res = -1;
-        else if (p1.getData().isBefore(p2.getData()))
+        if(p1.getData().isAfter(p2.getData()))
             res = 1;
+        else if(p1.getData().isBefore(p2.getData()))
+            res = -1;
         else return p1.getTitulo().compareTo(p2.getTitulo());
 
         return res;
     }
-
 }
